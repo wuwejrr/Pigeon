2cad3751edcfd1b9a951406be339de9aa954690d
wuwejrr <fanshou1agn@126.com>
2021-02-03 20:54:45 +0800

    修改入口文件

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import {Login} from './src/component/Login';
 
-AppRegistry.registerComponent('Pigeon', () => App);
+AppRegistry.registerComponent('Pigeon', () => Login);
