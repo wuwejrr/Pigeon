1f5efad4cc08e1062c64e1d8b6f546c2b57f701e
wuwejrr <fanshou1agn@126.com>
2021-02-03 22:53:23 +0800

    添加redux的action和reducers文件夹

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+
+// export const registerUser = (username,password) =>{
+//     return{
+//         type:"REGISTER_USER",
+//         username:username,
+//         password:password
+//     }
+// }
---
+++ src/reducers/register.js
@@ -0,0 +1,11 @@
+
+// const register = (state, action) =>{
+//     switch (action.type){
+//         case "REGISTER_USER":
+//             return{
+
+//             }
+//         default:
+//             return state;
+//     }
+// }
