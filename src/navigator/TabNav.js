69035fb0911aa800d78147c6b7d048c60cc397fa
wuwejrr <fanshou1agn@126.com>
2021-03-16 16:55:47 +0800

    修改tab导航器的显示样式

--- src/navigator/TabNav.js
+++ src/navigator/TabNav.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { TabNavigator } from 'react-navigation';
-import SessionList from '../page/SessionList';
-import FriendList from '../page/FriendList';
+import SessionList   from '../page/SessionList';
+import FriendList    from '../page/FriendList';
+import RequestList   from '../page/RequestList';
 
 export default TabNav = TabNavigator({
     SessionList:{ 
@@ -16,6 +17,13 @@ export default TabNav = TabNavigator({
             tabBarLabel: '好友',
         }
     },
+    RequestList:{
+        screen:RequestList,
+        navigationOptions:{
+            tabBarLabel: '好友申请',
+        }
+    }
 },{
     tabBarPosition: 'bottom',
+    inactiveBackgroundColor: 'white',
 });
