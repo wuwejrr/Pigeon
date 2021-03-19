fde6e52255440427099034dba4bc40eb8f0ff555
wuwejrr <fanshou1agn@126.com>
2021-03-19 11:48:39 +0800

    添加注释

--- src/util/ApiUtil.js
+++ src/util/ApiUtil.js
@@ -1,6 +1,10 @@
-
+/**
+ * 
+ * @param {接口类型} type 
+ * @param {请求方法} url 
+ * @param {数据} data 
+ */
 export function apiutil(type, url, data){
     var json = {"type":type,"url":url,"data":data};
-    console.log(json);
-    return json;
+    return JSON.stringify(json);
 }
