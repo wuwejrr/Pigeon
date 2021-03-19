ac6509d07b083352d8a4d13fd5df44dd971ad204
wuwejrr <fanshou1agn@126.com>
2021-03-19 11:46:48 +0800

    监听back键事件

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -1,10 +1,20 @@
 import React,{ Component } from 'react';
-import { View, StyleSheet, Button,Text } from 'react-native';
+import { View, StyleSheet, Button, Text, BackHandler, ToastAndroid } from 'react-native';
 import TabNav from '../navigator/TabNav';
 
 export default class MainPage extends Component {
     constructor(props){
         super(props);
+        BackHandler.addEventListener('handwareBackPress', this.onBackPress);
+    }
+    onBackPress = () => {
+        if (this.lastBackPressed && this.lastBackPressed + 2000 >= Date.now()) {
+            //最近2秒内按过back键，可以退出应用。
+            BackHandler.exitApp();
+          }
+        this.lastBackPressed = Date.now();
+        ToastAndroid.show('再按一次退出应用',ToastAndroid.SHORT);
+        return true;
     }
     render() {
         return(
@@ -15,7 +25,7 @@ export default class MainPage extends Component {
                         <Button
                             title='添加好友'
                             onPress={()=>
-                                this.props.navigation.navigate('AddFriend')}/>
+                                this.props.navigation.navigate('SearchFriend')}/>
                     </View>
                 </View>
                 <TabNav navigation={this.props.navigation}/>
