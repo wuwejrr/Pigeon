42a1ea1634c82c4d04922f1fc8716292eb609e2d
wuwejrr <fanshou1agn@126.com>
2021-04-11 22:39:51 +0800

    添加storage

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
 import NavHome from './src/navigator/NavHome';
+require('./src/util/Storage');
 
 AppRegistry.registerComponent('Pigeon', () => NavHome);
